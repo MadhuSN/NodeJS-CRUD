397055fd3bcfea032053536134500ad584685f77
Madhusudan N <madhu6258@gmail.com>
2019-06-02 17:47:14 +0530

    adding Files

---
+++ models/db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://test:test@mycluster1-kqs7r.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log('MongoDB Connection Succeeded.') }
+    else { console.log('Error in DB connection : ' + err) }
+});
+
+require('./employee.model');
